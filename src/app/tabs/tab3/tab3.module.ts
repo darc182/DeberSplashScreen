e153195d33bf2c20777af6e795be814f60dd658e
Andres <andresftma@gmail.com>
2025-05-29 16:05:15 -0500

    Implement tabbed navigation with three tabs and corresponding components

---
+++ src/app/tabs/tab3/tab3.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { Tab3Page } from './tab3.page';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule.forChild([{ path: '', component: Tab3Page }])
+  ],
+  declarations: [Tab3Page]
+})
+export class Tab3PageModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,12 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-
-// Importa Ionic PWA Elements
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
-if (environment.production) {
-  enableProdMode();
-}
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
     provideRouter(routes),
-  ],
-}).then(() => {
-  // Llama al loader después de que la aplicación se haya iniciado
-  defineCustomElements(window);
+    provideIonicAngular({})
+  ]
 });
